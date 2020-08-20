f94ee0ddc9e6655250b7f4ef9c5d4c7d3542cfad
koharakouki <ko.kohara02@gmail.com>
2020-08-20 23:44:20 +0900

    [fin] problem010

--- problem010.rb
+++ problem010.rb
@@ -1,2 +1,20 @@
 # 10以下の素数の和は 2 + 3 + 5 + 7 = 17 である.
 # 200万以下の全ての素数の和を求めよ.
+
+require 'prime'
+
+max = 2000000
+
+sum = 0
+
+(1..max).each do |int|
+  if int.prime?
+  	sum += int
+  end
+end
+
+p sum
+
+# これでもいける
+# p Prime.each(2000000).to_a.sum
+
