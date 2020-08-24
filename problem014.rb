a6cf61352f14d8305ece60396d0f6240e1346188
koharakouki <ko.kohara02@gmail.com>
2020-08-24 14:26:30 +0900

    [fin] problem014

--- problem014.rb
+++ problem014.rb
@@ -7,3 +7,27 @@
 # まだそのことは証明されていない(コラッツ問題)
 # さて, 100万未満の数字の中でどの数字からはじめれば最長の数列を生成するか.
 # 注意: 数列の途中で100万以上になってもよい
+
+# nが偶数の場合、奇数の場合でそれぞれifで分岐して数列の長さを求めるメソッド
+def collatz_sequence(number: , count:)
+	if number == 1
+		count += 1
+	elsif number.even?
+		collatz_sequence(number: number / 2, count: count += 1)
+	else
+		collatz_sequence(number: 3 * number + 1, count: count += 1)
+	end
+end
+
+result_hash = { longest_chain_count: 0, answer: 0 }
+
+(1..10).each do |number|
+	calculation = collatz_sequence(number: number, count: 0)
+	p calculation
+	if result_hash[:longest_chain_count] < calculation
+		result_hash[:longest_chain_count] = calculation
+		result_hash[:answer] = number
+	end
+end
+
+p result_hash
