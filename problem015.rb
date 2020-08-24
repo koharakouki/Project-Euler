5b5f98ce272c07813c0b26ddb8dd622d9e242760
koharakouki <ko.kohara02@gmail.com>
2020-08-25 08:18:17 +0900

    [fin] problem015

--- problem015.rb
+++ problem015.rb
@@ -1,2 +1,15 @@
 # 格子の経路、20 * 20 のパターンは何通りか？
 # 2 * 2の場合：→２個、↓２個 4!/2!*2! = 6
+
+# 階乗の計算をどう表現するか？
+
+# numberに数字を入れるとその階乗を返すメソッド
+def factorial(number)
+	(1..number).inject(:*)
+end
+
+num = 20
+
+p factorial(num + num) / (factorial(num) * factorial(num))
+
+
