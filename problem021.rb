130aac903fb586290e8a3fd11c53d047d49cb52f
koharakouki <ko.kohara02@gmail.com>
2020-08-31 10:09:15 +0900

    [fin] problem021

--- problem021.rb
+++ problem021.rb
@@ -5,3 +5,28 @@
 # また, 284 の約数は 1, 2, 4, 71, 142 なので d(284) = 220 である.
 
 # それでは10000未満の友愛数の和を求めよ.
+
+require 'prime'
+
+# 約数の和からnを引いた値を返すメソッド
+# def sum_divisors(n)
+#   (Prime.prime_division(n).map {|p, e| (p ** (e + 1) - 1) / (p - 1) }.inject(:*)) - n
+# end
+
+
+def sum_divisors(num)
+  (1..num).select{|i| num % i == 0 && num != i}.sum
+end
+
+
+sum = 0
+
+1.upto(9999) do |i|
+	div_a = sum_divisors(i)
+	div_b = sum_divisors(div_a)
+	if div_b == i && i != div_a
+		sum += i
+	end
+end
+
+p sum
