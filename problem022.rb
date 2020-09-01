0a875775a30bcb75ca177cf7e482d5600a58e6ff
koharakouki <ko.kohara02@gmail.com>
2020-09-01 09:27:30 +0900

    [fin] problem022

--- problem022.rb
+++ problem022.rb
@@ -3,3 +3,18 @@
 # たとえば, リストがアルファベット順にソートされているとすると, COLINはリストの938番目にある.
 # またCOLINは 3 + 15 + 12 + 9 + 14 = 53 という値を持つ. よってCOLINは 938 × 53 = 49714 というスコアを持つ.
 # ファイル中の全名前のスコアの合計を求めよ.
+
+array = []
+
+file = File.open("names.txt", "r") { |f| f.read.gsub('"','').split(",") }.sort
+
+sum = 0
+code = 'A'.bytes.to_a[0] - 1
+
+file.each_with_index do |n, i|
+	s = 0
+	n.each_byte { |x| s += x - code }
+	sum += (i+1) * s
+end
+
+p sum
