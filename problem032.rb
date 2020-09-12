e835d96dac086a536692c81a1d1466dae2dca8cb
koharakouki <ko.kohara02@gmail.com>
2020-09-12 14:36:53 +0900

    [fin] problem032

--- problem032.rb
+++ problem032.rb
@@ -4,3 +4,48 @@
 # 掛けられる数/掛ける数/積が1から9のパンデジタルとなるような積の総和を求めよ.
 # HINT: いくつかの積は, 1通り以上の掛けられる数/掛ける数/積の組み合わせを持つが1回だけ数え上げよ.
 
+
+def pandegital?(result)
+  num_chk = (1..9).to_a
+  result_chk = result.split("").map(&:to_i).sort
+
+  num_chk == result_chk ? true : false
+end
+
+
+answer = []
+
+(1..100000).each do |x|
+  (1..100000).each do |y|
+    result = x.to_s + y.to_s + (x * y).to_s
+    if result.size == 9 && pandegital?(result)
+      answer.push(x * y)
+      break
+    elsif result.size >= 10
+      break
+    end
+  end
+end
+
+p answer.uniq.sum
+
+
+
+digits = 9
+
+ans = []
+(1..digits).to_a.permutation(digits) do |a|
+  0.upto(digits - 3) do |i|
+    i.upto(digits - 3) do |j|
+      x = a[0..i].join.to_i
+      y = a[(i + 1)..(j + 1)].join.to_i
+      z = a[(j + 2)..(digits - 1)].join.to_i
+      if x * y == z
+        p "#{x} * #{y} = #{z}"
+        ans << z 
+      end
+    end
+  end
+end
+
+p ans.uniq.inject(:+)
