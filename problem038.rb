3718cf234b474db279691fe0e5fe3536bc5676cc
koharakouki <ko.kohara02@gmail.com>
2020-09-18 13:40:49 +0900

    [fin] problem038

--- problem038.rb
+++ problem038.rb
@@ -11,3 +11,24 @@
 # パンデジタル数 918273645 が得られる. これは 9 と (1,2,3,4,5) との連結積である.
 
 # 整数と (1,2,...,n) (n > 1) との連結積として得られる9桁のパンデジタル数の中で最大のものはいくつか?
+
+def get_pandigital? n
+  nums = []
+  (1..9).each do |digit|
+    nums += (n * digit).to_s.split ''
+    return 0 if nums.size != nums.uniq.size || nums.include?('0')
+    return nums.join('').to_i if nums.size == 9
+  end
+end
+
+solution = 0
+
+(9..9_876).each do |n|
+  # could do better by only looking at 9's!
+  result = get_pandigital? n
+  if result > solution
+    solution = result
+  end
+end
+
+puts solution
