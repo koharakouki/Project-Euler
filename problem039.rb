5bc13e675a0b365f4af2cdabfd03478f761cd9ca
koharakouki <ko.kohara02@gmail.com>
2020-09-19 11:34:33 +0900

    [fin] problem039

--- problem039.rb
+++ problem039.rb
@@ -4,3 +4,32 @@
 # {20,48,52}, {24,45,51}, {30,40,50}
 
 # p ≤ 1000 のとき解の数が最大になる p はいくつか?
+
+# a**2 + b**2 = c**2
+
+kome = []
+
+(1..500).each do |i|
+	(1..500).each do |j|
+		(1..500).each do |k|
+			if i**2 + j**2 == k**2 && i + j + k <= 1000
+				array = []
+				array << i
+				array << j
+				array << k
+				kome << array
+			end
+		end
+	end
+end
+
+sum = 0
+
+kome.sort.last.each do |i|
+  sum += i
+end
+
+p sum
+
+
+
