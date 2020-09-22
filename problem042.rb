ed39eec193acaadd6aaa23b2ec0453bd5aac09e7
koharakouki <ko.kohara02@gmail.com>
2020-09-22 13:24:45 +0900

    [fin] problem042

--- problem042.rb
+++ problem042.rb
@@ -8,3 +8,32 @@
 # 単語の値が三角数であるとき, その単語を三角語と呼ぶ.
 
 # 16Kのテキストファイル words.txt 中に約2000語の英単語が記されている. 三角語はいくつあるか?
+
+@triangle, i = [], 0
+while i <= 1000
+  i += 1
+  @triangle << i*(i+1)/2
+end
+
+@alphabet = ("A".."Z").to_a
+@sum = 0
+
+def search_triangle(word)
+  word.split("").each do |f|
+  @sum += @alphabet.index(f) + 1
+  end
+  result,@sum = @triangle.include?(@sum), 0
+  result
+end
+
+result = File.open("./p042_words.txt")
+lines = []
+result.each_line do |line|
+  lines << line.delete("\"")
+end
+lines = lines.join.split(",")
+
+count = 0
+lines.each do |g|
+  count += 1 if search_triangle(g)
+end
