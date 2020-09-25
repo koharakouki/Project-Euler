d7502158b4afc8f1f4265d461fbbabbc86e7eba5
koharakouki <ko.kohara02@gmail.com>
2020-09-25 21:34:20 +0900

    [fin] problem045

--- problem045.rb
+++ problem045.rb
@@ -6,3 +6,32 @@
 # # T285 = P165 = H143 = 40755であることが分かる.
 
 # # 次の三角数かつ五角数かつ六角数な数を求めよ.
+
+# 2 3 4 5
+# 4 7 10 13
+# 5 9 13 17
+
+class Integer
+  # * 五角数であれば、何番目かを返す。
+  def pentagon?
+    x = ((1 + 24 * self) ** 0.5 + 1) / 6.0
+    if x == x.to_i
+      return x.to_i
+    else
+      return false
+    end
+  end
+
+  # * 六角数を求める。
+  def hexagon
+    return 2 * self * self - self
+  end
+end
+
+144.upto(1/0.0) do |i|
+  h = i.hexagon
+  if h.pentagon? # 両方条件を満たすなら
+    puts h
+    exit
+  end
+end
