7410e0ca3cc93a61eb82735ac50f3394bd4dfe5d
koharakouki <ko.kohara02@gmail.com>
2020-09-29 17:19:45 +0900

    [fin] problem049

--- problem049.rb
+++ problem049.rb
@@ -5,3 +5,11 @@
 # 1, 2, 3桁の素数にはこのような性質を持った数列は存在しないが, 4桁の増加列にはもう1つ存在する.
 
 # それではこの数列の3つの項を連結した12桁の数を求めよ.
+
+require "prime"
+
+arr = (1000..9999).to_a.select!{ |a| a.prime? && a <= 3330 }
+
+arr.select!{|num| num if num.to_s.split("").sort == (num+3330).to_s.split("").sort && (num+3330).prime? && (num+6660).prime? }
+
+p arr.last.to_s + (arr.last+3330).to_s + (arr.last+6660).to_s
