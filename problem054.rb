113bf7938f72c391ba82b7a557349d13da0585e6
koharakouki <ko.kohara02@gmail.com>
2020-10-04 05:54:45 +0900

    [fin] problem055

--- problem054.rb
+++ problem054.rb
@@ -17,3 +17,27 @@
 # 驚くべきことに, 回文数かつLychrel数であるものが存在する. 最初の数は4994である.
 
 # 10000未満のLychrel数の個数を答えよ.
+
+def parindrome?(num)
+  return true if num.to_s == num.to_s.reverse
+  false
+end
+
+def sum_add_reverse(num)
+  num + num.to_s.reverse.to_i
+end
+
+def lychrel?(num)
+  buf = num
+  49.times do |i|
+    return false if parindrome?(buf = sum_add_reverse(buf))
+  end
+  true
+end
+
+answer = 0
+1.upto(9999) do |i|
+  answer += 1 if lychrel?(i)
+end
+
+p answer
